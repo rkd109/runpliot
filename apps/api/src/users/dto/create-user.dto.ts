b7390b917600ca5f501a65630f96f55ad0f8fe2d
jingu_m <rkd109@naver.com>
2026-05-14 13:47:42 +0900

    validate 추가

--- apps/api/src/users/dto/create-user.dto.ts
+++ apps/api/src/users/dto/create-user.dto.ts
@@ -1,12 +1,18 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsString, IsEmail, MinLength } from "class-validator";
 
-export class CreateUserDto{
+export class CreateUserDto {
+    @IsEmail()
     @ApiProperty({ example: 'test@example.com', description: '이메일'  })
     email!: string;
 
+    @IsString()
+    @MinLength(2)
     @ApiProperty({ example: 'jingu' , description: '별명' })
     nickname!: string;
 
-    @ApiProperty({ example: 'hash-password', description: '이메일'  })
-    passwordHash!: string;
+    @IsString()
+    @MinLength(8)
+    @ApiProperty({ example: 'password', description: '이메일'  })
+    password!: string;
 }
--- apps/api/src/users/dto/update-user.dto.ts
+++ apps/api/src/users/dto/update-user.dto.ts
@@ -1,7 +1,21 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail, IsOptional, IsString, MinLength } from "class-validator";
 
 export class UpdateUserDto{
-    @ApiProperty({ example: 'jingu' })
+    @IsOptional()
+    @IsEmail()
+    @ApiProperty({ example: 'test@example.com', description: '이메일'  })
+    email?: string;
 
+    @IsOptional()
+    @IsString()
+    @MinLength(2)
+    @ApiProperty({ example: 'jingu' , description: '별명' })
     nickname? : string;
+
+    @IsOptional()
+    @IsString()
+    @MinLength(8)
+    @ApiProperty({ example: 'password', description: '이메일'  })
+    password?: string;
 }
